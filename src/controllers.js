db5d0f6afe70e37a7c34241ec174abaca43dd588
Jaider <jaidercueto.1010@gmail.com>
2023-06-21 09:29:13 -0500

    controllers.js file added

---
+++ src/controllers.js
@@ -0,0 +1,80 @@
+let axios = require('axios')
+require('dotenv').config();
+const {
+    API_DOMAIN,
+    YOUR_API_KEY
+} = process.env;
+
+
+const dietsObj = {
+    "gluten free": false,
+    "ketogenic": false,
+    "vegetarian": false,
+    "lacto-vegetarian": false,
+    "ovo-vegetarian": false,
+    "vegan": false,
+    "pescetarian": false,
+    "paleo": false,
+    "paleolithic": false,
+    "primal": false,
+    "low FODMAP": false,
+    "whole30": false
+}
+
+const mainDataFunction = (recipe) => {
+    return {
+        id: recipe.id,
+        name: recipe.title,
+        image: recipe.image,
+        dishType: recipe.dishTypes,
+        summarizeDish: recipe.summary,
+        healthScore: recipe.healthScore,
+        steps: !recipe.analyzedInstructions.length ?
+            recipe.analyzedInstructions[0] :
+            recipe.analyzedInstructions[0].steps.map(steps => `${steps.number}. ${steps.step}`),
+        diets: recipe.diets
+    }
+}
+
+const createDiets = () => {
+    let formatForDb = Object.keys(dietsObj).map(item => {
+        return {
+            name: item
+        }
+    })
+    return formatForDb
+}
+
+const getRecipes = async() => {
+        let response = await axios.get(`
+            ${API_DOMAIN}complexSearch?apiKey=${YOUR_API_KEY}&number=${9}&addRecipeInformation=true`
+        )
+        if(response.hasOwnProperty('status')){
+            let respuesta = await response.data.results
+            let mainData = respuesta.map(recipe => mainDataFunction(recipe))
+            return mainData
+        }else{
+            return {
+                error: response.message
+            }
+        }
+        
+}
+
+const getRecipeById = async(idReceta) => {
+        let response = await axios.get(`${API_DOMAIN}${idReceta}/information?apiKey=${YOUR_API_KEY}`)
+        let respuesta = await response.data
+        
+        if(!response.hasOwnProperty('code')){
+            const res = mainDataFunction(respuesta)
+            return res
+        }else{
+            return {error: res.message}
+        }
+}
+
+module.exports = {
+    createDiets,
+    getRecipes,
+    getRecipeById
+}
