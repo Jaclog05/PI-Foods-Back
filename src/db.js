d80f4fbed148817be6973de0de33ebf2bf80ca95
Jaider <jaidercueto.1010@gmail.com>
2026-07-07 11:27:51 -0500

    database deployed in Neon instead of elephantsql. Connection URL in .env

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY
+  DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY_NEON
 } = process.env;
 
 /* const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/food`, {
@@ -11,12 +11,7 @@ const {
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 }); */
 
-/* const sequelize = new Sequelize(DB_DEPLOY, {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-});
-const basename = path.basename(__filename); */
-const sequelize = new Sequelize("postgres://lbeenbwa:p-yp31aHwoHspmXTWoyifCVeqrpXeWhd@motty.db.elephantsql.com/lbeenbwa", {
+const sequelize = new Sequelize(DB_DEPLOY_NEON, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
